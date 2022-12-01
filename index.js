29d740672da6f6a22776a0a3647ba221f4d54528
isaac <isaactonyloi@gmail.com>
2022-12-01 14:30:17 +0300

    js events

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 function addingEventListener() {
+    const input = document.getElementById("input");
+    function clickAlert() {
+        alert("You know I was clicked !");
+    }
+    
+    input.addEventListener('click', clickAlert);
+
+   
 }
+
+addingEventListener();
+
+
+
+
+
